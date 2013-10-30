83fb5723af73284c47cbca35cc67f25e57468dcd
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2013-10-30 11:33:13 +0100

    update functionsphp

--- functionsphp.py
+++ functionsphp.py
@@ -3,10 +3,10 @@ import sublime, sublime_plugin, header
 class insert_functionsphpCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.view.set_name('functions.php')
-		self.view.set_syntax_file('../PHP/PHP.tmLanguage')
+		# self.view.set_syntax_file('../PHP/PHP.tmLanguage')
 
 		try:
-			with open('functions.php.txt', 'rb') as fp:
+			with open('functionsphp.txt', 'r') as fp:
 				self.view.run_command('insert_header', {'contents' : fp.read() })
 		except IOError:
 			print 'bugger'
