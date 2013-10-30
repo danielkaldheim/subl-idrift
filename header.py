c28def87ec7065cff3621d75a1053305f91fae1a
Daniel Rufus Kaldheim <daniel@kaldheim.org>
2013-10-30 12:24:24 +0100

    removed try

--- header.py
+++ header.py
@@ -135,9 +135,5 @@ class insert_functionsphpCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.view.set_name('functions.php')
 		# self.view.set_syntax_file('../PHP/PHP.tmLanguage')
-
-		try:
-			with open('functionsphp.txt', 'r') as fp:
-				self.view.run_command('insert_header', {'contents' : fp.read() })
-		except IOError:
-			pass
+		with open('functionsphp.txt', 'r') as fp:
+			self.view.run_command('insert_header', {'contents' : fp.read() })
